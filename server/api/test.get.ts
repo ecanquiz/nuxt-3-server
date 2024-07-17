9b5cd70c5a332650d119f7e09b33e6e55b7bfef0
Ernesto Canquiz <caribestic123@gmail.com>
2024-07-17 15:20:10 -0400

    feat:  Server utils & auto import files

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -1,4 +1,10 @@
+// import myFunction from "../utils/my-function"
+// import testing from "../test/testing"
+
 export default defineEventHandler((event)=>{
   console.log(event.context.authorized)  
-  return 'Hello World with GET method'
+  // return 'Hello World with GET method'
+  // return myFunction()
+  // return testing()
+  return `Hello World with GET method / ${myFunction()} / ${testing()}`
 })
