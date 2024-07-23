7ee7ed5a10acd47543284c4a71358b8f182702c7
Ernesto Canquiz <caribestic123@gmail.com>
2024-07-23 10:34:30 -0400

    feat: Explore h3 utilities

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -2,9 +2,22 @@
 // import testing from "../test/testing"
 
 export default defineEventHandler((event)=>{
-  console.log(event.context.authorized)  
-  // return 'Hello World with GET method'
-  // return myFunction()
-  // return testing()
-  return `Hello World with GET method / ${myFunction()} / ${testing()}`
+  try {
+    console.log(event.context.authorized)  
+    // return 'Hello World with GET method'
+    // return myFunction()
+    // return testing()  
+    setResponseHeader(event, 'Authorization', 12)
+    setCookie(event, 'cookie-name', 'cookie-value', {
+      httpOnly: true,
+      expires: new Date( new Date().setDate(new Date().getDate() + 30))
+    })
+    
+    // return parseCookies(event)
+    // deleteCookie(event, 'cookie-name')
+    return `Hello World with GET method / ${myFunction()} / ${testing()} / ${getCookie(event, 'cookie-name')}` 
+  
+  } catch (error) {
+    return createError({statusCode: 500, statusMessage: 'Error in your API.'})
+  }
 })
